95695f82d46b7f9e7d942526bcaccb43e21f0ffe
oakwoodyk <yuka1217kimura86@gmail.com>
2021-11-06 00:09:05 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/* 問題① */
+const array = [2, 4, 7, 5, 4, 3, 8];
+const result = array.filter(function (x, i, self) {
+  return self.indexOf(x) === i;
+});
+console.log(result);
+
+
+
+
+/* 問題② */
+const a = [2020, 2021];
+function leapYear(a){
+  if (( a % 4 == 0) && (a % 100 !== 0)) {
+    return `${a}`+"年は、うるう年です";
+    }
+  else if (a % 400 == 0) {
+      return `${a}`+"年は、うるう年です";
+    }
+  else {
+      return `${a}`+"年は、うるう年ではありません";
+    }
+  }
+console.log(leapYear(a[0]));
+console.log(leapYear(a[1]));
+
